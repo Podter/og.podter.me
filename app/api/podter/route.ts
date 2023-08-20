f6b5740a9720d8822201534c474210b543213435
Nonthaphat Chaisu <48936015+Podter@users.noreply.github.com>
2023-08-20 12:19:23 +0700

    working og

--- app/api/podter/route.ts
+++ app/api/podter/route.ts
@@ -1,11 +1,36 @@
 import { ImageResponse } from "next/server";
-import Podter from "./podter";
+import Podter from "./og";
 
 export const runtime = "edge";
 
-export async function GET() {
-  return new ImageResponse(Podter(), {
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const title = searchParams.get("title");
+  const date = searchParams.get("date");
+
+  const switzerRegular = await fetch(
+    new URL("./switzer-regular.ttf", import.meta.url),
+  ).then((res) => res.arrayBuffer());
+  const switzerBold = await fetch(
+    new URL("./switzer-bold.ttf", import.meta.url),
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(Podter({ title, date }), {
     width: 1200,
     height: 630,
+    fonts: [
+      {
+        name: "Switzer",
+        data: switzerRegular,
+        style: "normal",
+        weight: 400,
+      },
+      {
+        name: "Switzer",
+        data: switzerBold,
+        style: "normal",
+        weight: 700,
+      },
+    ],
   });
 }
